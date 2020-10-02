80b63fc1433c9b1360e3180c9360bf1f6330bb6a
jacoe0850 <jacoe0850@wrdsb.ca>
2020-10-02 23:10:42 +0000

    Finished count down integer

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @jacoe0850 
  */
 public class Main {
 
@@ -9,7 +10,14 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter an integer to count down to");
+    int downTo = input.nextInt();
+    int count = 100;
+    while(count >= downTo){
+      System.out.println(count);
+      count = count - 5;
+    }
     
   }
 }
